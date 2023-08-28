a78fdaec9a29ecd2902f744c5a37557882e75bcb
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2023-08-28 08:59:26 -0400

    Implement async main function

--- js/main.js
+++ js/main.js
@@ -15,51 +15,14 @@ const state = {
   },
 }
 
-const meteoriteData = getData();
-console.log(meteoriteData); */
+async function main() {
+  // Run dark mode module
+  darkMode()
 
-/* const htmlContentArray = meteoriteData.map((meteorite) => {
-  return `
-<tr>
-						<td data-cell="input"><input type="checkbox" name="id-number" id="id"></td>	
-						<td data-cell="id">1</td>
-						<td data-cell="name">${meteorite.name}</td>
-						<td data-cell="year">Snow</td>
-						<td data-cell="meteorite">26</td>
-						<td data-cell="size">Jon Snow</td>
-						<td></td>
-						<td></td>
-						<td></td>
-					</tr>
-
-`;
-}); */
-//console.log(htmlContentArray());
-const darkModeToggle = document.getElementById("dark-mode-toggle");
-const displayMode = document.getElementById("display-mode");
-const body = document.body;
-
-darkModeToggle.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
-
-  if (body.classList.contains("dark-mode")) {
-    darkModeToggle.classList.remove("fa-toggle-on");
-    darkModeToggle.classList.add("fa-toggle-off");
-  } else {
-    darkModeToggle.classList.remove("fa-toggle-off");
-    darkModeToggle.classList.add("fa-toggle-on");
-  }
-});
-
-//Selecting all checboxes
-const allCheckbox = document.getElementById("all-checkbox");
-
-allCheckbox.addEventListener("change", () => {
-  const checkboxList = document.getElementsByName("id-number");
-  for (i of checkboxList) {
-    allCheckbox.checked ? (i.checked = true) : (i.checked = false);
-  }
-});
+  // Fetch meteorites data and display populated table
+  state.meteorites = await fetchData(state, API_URL)
+  displayTable(state, document.querySelector("table"))
+}
 
 //Clear all input fields
 const clearBtn = document.getElementById("clear-btn");
