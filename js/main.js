e40de620f824dbc23fe0a12b0ebde75adf8c68d9
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2023-08-28 09:15:21 -0400

    Call main function

--- js/main.js
+++ js/main.js
@@ -32,15 +32,5 @@ async function main() {
   clearInputs()
 }
 
-//Clear all input fields
-const clearBtn = document.getElementById("clear-btn");
-const searchForm = document.getElementById("search-form");
-const searchInput = document.getElementsByTagName("input");
-
-clearBtn.addEventListener("click", () => {
-  for (i in searchInput) {
-    i.value = "";
-  }
-});
-
 //Input Validation and Error Message
+main()
